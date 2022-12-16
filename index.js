b590f2fe728a13308111a128419dd85b58780b2f
LNSY <lindsey.asterius@gmail.com>
2022-12-16 14:17:04 -0800

    removed comments

--- index.js
+++ index.js
@@ -5,20 +5,16 @@
 class EasyForm extends HTMLElement {
   connectedCallback(){
     const submit = document.querySelector('input[type="submit"');
-    console.log(submit);
     if(submit !== null){
       submit.addEventListener('click', (e)=>{
         e.preventDefault(); 
         const values = this.getFormValues();
-        console.log(values)
         this.dispatchEvent(new CustomEvent('submit', {detail: values}))
       })
     }
-
   }
 
   getFormValues(){
-
     let values = {};
     [...this.querySelectorAll('input')].forEach(el => {
       if(el.name === "" && el.type !== 'submit') return console.error('All inputs need an Name attribute');
@@ -38,7 +34,6 @@ class EasyForm extends HTMLElement {
     });
     return values;
   }
-
 }
 
 customElements.define('easy-form', EasyForm)
